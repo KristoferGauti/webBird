345bf84743ebbf8d909719b114079f1ed5afd0d3
KristoferGauti <kristofergauti@gmail.com>
2022-08-14 23:24:58 +0000

    TODO list draft created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
 import Header from "../components/header/header";
 
+/**
+ * TODO:
+ * 1) Create a fully functional flappy bird
+ * 2) Create a dark mode toggle button in the header component
+ * 3) Use some kind of reinforcement learning algorithm to learn to play Flappy bird optimally
+ */
+
 const Home: NextPage = () => {
     return (
         <>
